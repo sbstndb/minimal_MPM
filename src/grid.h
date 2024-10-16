18a71d888747cf4f9e2169285f9eebfb1d1ee65e
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-10-16 02:44:33 +0200

    Add very minimal grid class

---
+++ src/grid.h
@@ -0,0 +1,35 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <iostream>
+
+class Node {
+public:
+	Node() ; 
+	~Node() ; 
+
+
+	
+};
+
+
+
+class Grid {
+public:
+	Grid() ; 
+	~Grid() ; 
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
