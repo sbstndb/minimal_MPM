5dd9c808e7fa43daba186c81de10dc47231972cb
sbstndb <30315405+sbstndb@users.noreply.github.com>
2024-10-16 21:21:58 +0200

    Add grid class
    	-	Cuda arrays
    	-	TODO : initialize values

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,3 +1,47 @@
+#include <iostream>
 #include "grid.h"
+
+Grid::Grid(){} ; 
+Grid::~Grid(){
+	std::cout << "Desctror grid " << std::endl ; 
+        cudaFree((void*)d_x);
+        cudaFree((void*)d_y);
+        cudaFree((void*)d_z);
+
+        cudaFree((void*)d_vx);
+        cudaFree((void*)d_vy);
+        cudaFree((void*)d_vz);
+
+        cudaFree((void*)d_px);
+        cudaFree((void*)d_py);
+        cudaFree((void*)d_pz);
+
+        cudaFree((void*)d_mass);
+};
+
+void Grid::addGrid(float dx, float dy, float dz, float h){
+
+};
+
+
+void Grid::allocateGrid(int nNodes){
+        std::cout << "Allocate grid " << std::endl ;
+
+        cudaMalloc((void**)&d_x, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_y, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_z, nNodes * sizeof(float)) ;
+
+        cudaMalloc((void**)&d_vx, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_vy, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_vz, nNodes * sizeof(float)) ;
+
+        cudaMalloc((void**)&d_px, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_py, nNodes * sizeof(float)) ;
+        cudaMalloc((void**)&d_pz, nNodes * sizeof(float)) ;
+
+        cudaMalloc((void**)&d_mass, nNodes * sizeof(float)) ;
+
+};
+
 
 
--- src/grid.h
+++ src/grid.h
@@ -2,15 +2,7 @@
 #define GRID_H
 
 #include <iostream>
-
-class Node {
-public:
-	Node() ; 
-	~Node() ; 
-
-
-	
-};
+#include <cuda_runtime.h>
 
 
 
@@ -18,7 +10,23 @@ class Grid {
 public:
 	Grid() ; 
 	~Grid() ; 
+
+	void addGrid(float, float, float, float) ; 
+	void allocateGrid(int ) ; 
+
+	int nx, nt, nz;
+	float h; // hx = hy = hz for now
+
+	// cuda arrays 
+	// In reality we can use limited amount of arrays because you dont need them at th same time
+	float *d_x, *d_y, *d_z ; 
+	float *d_vx, *d_vy, *d_vz;
+	float *d_px, *d_py, *d_pz;
+	float *d_mass;
+
 };
+
+
 
 
 
